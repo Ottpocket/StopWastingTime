61ead968988f8ff0e9b757d30dbf0fcf45fde3a5
El Conde de Ñáñaño <64142335+Ottpocket@users.noreply.github.com>
2023-09-18 09:28:46 -0500

    Updated main.py

--- main.py
+++ main.py
@@ -6,3 +6,20 @@ How does the StopWastingTime app work?
 2) checks the internet every 1 second to see if there exist sites not on the whitelist
   2a)If so, closes all internet browsers
 """
+import sleep
+from WhiteList import WhiteListCoR
+from CheckWebsites import CheckWebsitesCoR
+from CourseCorrect import CourseCorrectCoR
+
+
+whitelist = WhiteListCor('base')
+website_checker = CheckWebsitesCoR('base')
+course_correction = CourseCorrectCoR('delete')
+
+while True:
+  if whitelist.website_in_whitelist(website_checker.get_current_websites()):
+    course_correction.course_correct()
+  else:
+    pass
+
+  time.sleep(1.)
