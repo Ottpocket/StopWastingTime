7c470b87ceaf6c670d639f47f814674555c926cb
El Conde de Ñáñaño <64142335+Ottpocket@users.noreply.github.com>
2023-09-18 08:45:10 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+Runs the StopWastingTime App
+
+How does the StopWastingTime app work?
+1) looks at whitelist.txt to find good websites
+2) checks the internet every 1 second to see if there exist sites not on the whitelist
+  2a)If so, closes all internet browsers
+"""
