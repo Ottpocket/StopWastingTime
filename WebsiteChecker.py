bc64a83218c623ef2d98a0c6fc6d58f213519594
El Conde de Ñáñaño <64142335+Ottpocket@users.noreply.github.com>
2023-09-19 07:23:55 -0500

    WebsiteCheckerOnline starting

--- WebsiteChecker.py
+++ WebsiteChecker.py
@@ -7,6 +7,9 @@ checker = WebsiteCheckerCoR().get_object('test')
 checker.get_current_websites()
 """
 from abc import ABC, abstractmethod
+import os
+import sqlite3
+import time
 
 class WebsiteChecker(ABC):
   """ Checks current websites """
@@ -17,8 +20,19 @@ class WebsiteChecker(ABC):
 
 
 class WebsiteCheckerOnline(WebsiteChecker):
+  """ Looks online via something to look at website tabs"""
   def get_current_websites(self):
-    
+    pass    
+
+class WebsiteCheckerHistory(WebsiteChecker):
+  """ Looks at local History sqlite3 database to check history.
+  NOTE: this assumes windows and only looks at Chrome
+  """
+  self.user = os.getlogin()
+  self.chrome_history = f"C:\\Users\\{self.user}\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\History"
+  self.pgm_start = time.time()
+  def get_current_websites(self):
+    pass
 
 class WebsiteCheckerTest(WebsiteChecker):
   """ Test class for debugging """
