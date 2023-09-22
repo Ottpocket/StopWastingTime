f0fce8ce8a6cb06c19e3ef5590f77b9d2560d60f
El Conde de Ñáñaño <64142335+Ottpocket@users.noreply.github.com>
2023-09-22 07:35:31 -0500

    WhiteListFromPids added

--- WhiteList.py
+++ WhiteList.py
@@ -1,6 +1,11 @@
 """
 Class for good websites
 """
+from abc import ABC, abstractmethod
+
+def get_pids(browser):
+  """ returns a list of all pids for a browser
+   GetPidsFactory().get_object().get_pids(browser=browser)"""
 
 
 class WhiteListCoR:
@@ -9,29 +14,46 @@ class WhiteListCoR:
   def get_object(self, factory_type):
     if factory_type == 'base':
       return WhiteListFromTxt()
-    if factory_type == 'test':
+    elif factory_type == 'test':
       return WhiteListTest()
+    elif factory_type == 'pids':
+      return WhiteListPids()
     else:
       raise Exception(f'{factory_type} not a proper parameter to create WhiteList object.')
 
-class WhiteList:
+class WhiteList(ABC):
   """ Creates a list of strings of good websites for browsing"""
   def __init__(self):
     self.whitelist = self.create_whitelist()
   
-
-  def create_whitelist(self, **kwargs) -> list:
+  @abstractmethod
+  def create_whitelist(self):
     pass
 
-  def website_in_whitelist(self, list_of_current_sites):
+  def website_in_whitelist(self, list_of_current_sites, whitelist_sites):
     """ checks if all current sites in whitelist """
+    bad_sites = []
     for site in list_of_current_sites:
-      if sum([sum([white_site.upper().startswith(site.upper()) for white_site in self.whitelist])]) ==0:
+      if sum([sum([white_site.upper().startswith(site.upper()) for white_site in whitelist_sites])]) ==0:
         print(f'{site} not found in whitelist')
-        return False
-    else:
-      return True
-      
+        bad_sites.append(site)
+    return bad_sites
+
+class WhiteListFromPids(WhiteList):
+  """ creates a whitelist from pids currently running
+
+  This whitelist has the form of the following dict:
+  { 
+    'chrome': [pid1, pid2, ...]
+    'firefox': [pid1, pid2, ...]
+    'msedge':[pid1, pid2, ...]
+  }
+  """
+  def create_whitelist(self):
+    whitelist = {}
+    for browser in ['firefox', 'chrome', 'msedge']:
+      whitelist[browser] = GetPidsFactory().get_object().get_pids(browser=browser)
+
 class WhiteListFromTxt(WhiteList):
   """ creates the whitelist of whitelist.txt """
 
